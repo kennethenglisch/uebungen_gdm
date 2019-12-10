f3ddf658736c96e87568d0c6141b56560df215a7
Kenneth <Kenneth@DESKTOP-8O89D8D.fritz.box>
2019-12-10 16:15:31 +0100

    - worked on Uebung 4

--- GRDM_U4_s0570727.java
+++ GRDM_U4_s0570727.java
@@ -10,7 +10,7 @@ import ij.plugin.filter.*;
 public class GRDM_U4_s0570727 implements PlugInFilter {
 
 	protected ImagePlus imp;
-	final static String[] choices = {"Wischen", "Weiche Blende", "Chroma Key", "Extra"};
+	final static String[] choices = {"Wischen", "Weiche Blende", "Overlay", "Schieb-Blende", "Chroma Key", "Extra", "Extra 2"};
 
 	public int setup(String arg, ImagePlus imp) {
 		this.imp = imp;
@@ -73,8 +73,11 @@ public class GRDM_U4_s0570727 implements PlugInFilter {
 		String s = gd.getNextChoice();
 		if (s.equals("Wischen")) methode = 1;
 		if (s.equals("Weiche Blende")) methode = 2;
-		if (s.equals("Chroma Key")) methode = 3;
-		if (s.equals("Extra")) methode = 4;
+		if (s.equals("Overlay")) methode = 3;
+		if (s.equals("Schieb-Blende")) methode = 4;
+		if (s.equals("Chroma Key")) methode = 5;
+		if (s.equals("Extra")) methode = 6;
+		if (s.equals("Extra 2")) methode = 7;
 
 		// Arrays fuer die einzelnen Bilder
 		int[] pixels_B;
@@ -102,26 +105,109 @@ public class GRDM_U4_s0570727 implements PlugInFilter {
 					int gB = (cB & 0x00ff00) >> 8;
 					int bB = (cB & 0x0000ff);
 
-					if (methode == 1)
+					if (methode == 1) // nun horizontal - changed x+1 -> y+1
 					{
-					if (x+1 > (z-1)*(double)width/(length-1))
+					if (y+1 > (z-1)*(double)width/(length-1))
 						pixels_Erg[pos] = pixels_B[pos];
 					else
 						pixels_Erg[pos] = pixels_A[pos];
 					}
 
-					/*
-					if (methode == 2)
-					{
-					// ...
 					
-					int r = ...
-					int g = ...
-					int b = ...
+					if (methode == 2) // Weiche Blende
+					{	/** nach Formel (Folie 47)
+						* 	@credits http://home.htw-berlin.de/~barthel/veranstaltungen/GLDM/vorlesungen/04_GLDM_Bildmanipulation1_Bildpunktoperatoren.pdf
+						*/
+						int trans = z - 1;
+						int one = length - 1;
+
+						int r = (trans * rA + (one - trans) * rB) / one;
+						int g = (trans * gA + (one - trans) * gB) / one;
+						int b = (trans * bA + (one - trans) * bB) / one;
 
 					pixels_Erg[pos] = 0xFF000000 + ((r & 0xff) << 16) + ((g & 0xff) << 8) + ( b & 0xff);
 					}
-					*/
+					
+					if (methode == 3) // Overlay
+					{ 	/** nach Formel (Folie 38)
+						* 	@credits http://home.htw-berlin.de/~barthel/veranstaltungen/GLDM/vorlesungen/04_GLDM_Bildmanipulation1_Bildpunktoperatoren.pdf
+						*/
+						int r, g, b;
+
+						if (rA <= 128)
+
+							r = rA * rB / 128;
+
+						else
+							r = 255 - (255 - rA) * (255 - rB) / 128;
+
+						if (gA <= 128)
+
+							g = gA * gB / 128;
+
+						else
+							g = 255 - (255 - gA) * (255 - gB) / 128;
+
+						if (bA <= 128)
+
+							b = bA * bB / 128;
+
+						else
+							b = 255 - (255 - bA) * (255 - bB) / 128;
+
+						pixels_Erg[pos] = 0xFF000000 + ((r & 0xff) << 16) + ((g & 0xff) << 8) + (b & 0xff);
+					}
+
+					if (methode == 4) // Schieb-Blende
+					{
+						int line = (z - 1) * width / (length - 1);
+
+						if (x + 1 > line) {
+
+							pixels_Erg[pos] = pixels_B[pos - line];
+						}
+
+						else
+
+							pixels_Erg[pos] = pixels_A[pos];
+					}
+
+					if (methode == 5) // Chroma - Key
+					{
+
+						if (rA > 150 && bA < 140)
+							pixels_Erg[pos] = pixels_B[pos];
+						else
+							pixels_Erg[pos] = pixels_A[pos];
+
+					}
+
+					if (methode == 6) // Jalousie
+					{
+
+						int distance = ((z - 1) * width / (length - 1));
+
+						int num = 16;
+
+						pixels_Erg[pos] = pixels_B[pos];
+
+						if (y % num < distance / ( num-1 ))
+							pixels_Erg[pos] = pixels_A[pos];
+					}
+					
+					if (methode == 7 ) // Extra
+					{
+						float xPlusYDividedByZ = ( width + height ) / ((float)length -1 );
+						
+						
+						int b =  (int) Math.ceil((z-1) * xPlusYDividedByZ) ;
+					
+						
+
+						if ( x + y > b ) pixels_Erg[pos] = pixels_B[pos];
+						else pixels_Erg[pos] = pixels_A[pos];
+					}
+					
 				}
 		}
 

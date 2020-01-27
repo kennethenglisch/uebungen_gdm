cb7ca788d3e8e81b374f9c22a1886611fcc8e409
Kenneth <Kenneth@DESKTOP-HH9SJBP.fritz.box>
2020-01-27 11:20:21 +0100

    - better

--- Scale_s0570727.java
+++ Scale_s0570727.java
@@ -5,209 +5,156 @@ import ij.gui.NewImage;
 import ij.plugin.filter.PlugInFilter;
 import ij.process.ImageProcessor;
 
-
 public class Scale_s0570727 implements PlugInFilter {
+	ImagePlus imp;
 
-	protected ImagePlus imp; // ImagePlus object
-	
-	public static void main(String[] args) {
+	public static void main(String args[]) {
+
 		IJ.open("./component.jpg");
 
-		Scale_s0570727 sd = new Scale_s0570727();
-        sd.imp = IJ.getImage();
-        ImageProcessor ip = sd.imp.getProcessor();
-        sd.run(ip);
-    }
- 
-    public int setup(String arg, ImagePlus imp) {
-        if (arg.equals("about"))
-        {showAbout(); return DONE;}
-        return DOES_RGB+NO_CHANGES;
-        // kann RGB-Bilder und veraendert das Original nicht
-    }
+		Scale_s0570727 sc = new Scale_s0570727();
+		sc.imp = IJ.getImage();
+		ImageProcessor B_ip = sc.imp.getProcessor();
+		sc.run(B_ip);
+	}
 
-    public void run(ImageProcessor ip) {
- 
-        String[] dropdownmenue = {"Kopie", "Pixelwiederholung", "Bilinear"};
-        GenericDialog gd = new GenericDialog("scale");
-        gd.addChoice("Methode",dropdownmenue,dropdownmenue[0]);
-        gd.addNumericField("Breite:",800,0);
-        gd.addNumericField("Hoehe:",700,0);
- 
-        gd.showDialog();
- 
-        int newWidth =  (int)gd.getNextNumber();
-        int newHight = (int)gd.getNextNumber(); // _n fuer das neue skalierte Bild
-        String choice = gd.getNextChoice();
- 
-        int width  = ip.getWidth();  // Breite bestimmen
-        int height = ip.getHeight(); // Hoehe bestimmen
-         
-         
-        ImagePlus scaledImage = NewImage.createRGBImage("Skaliertes Bild",
-                newWidth, newHight, 1, NewImage.FILL_BLACK);
- 
-        ImageProcessor ip_n = scaledImage.getProcessor();
- 
-        int[] pix = (int[])ip.getPixels();
-        int[] pix_n = (int[])ip_n.getPixels();
-        boolean test = false;
-        
-        if (choice == "Kopie") {
-            copyImage(pix, width, height, pix_n, newWidth, newHight);
-        } else if (choice == "Pixelwiederholung") {
-            nearestNeighbor(pix, width, height, pix_n, newWidth, newHight);
-        } else if (choice == "Bilinear") {
-            bilinearInterpolation(pix, width, height, pix_n, newWidth, newHight);
-           
-        }
-        // neues Bild anzeigen
-        if (!test) {
-            scaledImage.show();
-            scaledImage.updateAndDraw();
-        }
-    }
-     
-    private int pos(int x, int y, int width) {
-        int position = y*width +x;
-        return position;
-    }
- 
-    private void bilinearInterpolation(int[] origPix, int origWidth,
-            int origHeight, int[] newPix, int newWidth, int newHeight) {
-        // Werte -1, damit nicht über den Rand gelaufen wird
-        double oW = (double) (origWidth-1);
-        double nW = (double) (newWidth-1);
-        double oH = (double) (origHeight-1);
-        double nH = (double) (newHeight-1);
-        double ratioX = oW/nW;
-        double ratioY = oH/nH;
-        int rgbA = 0;
-        int rgbB = 0;
-        int rgbC = 0;
-        int rgbD = 0;
- 
-        // Schleife ueber das neue Bild
-        for (int yNew=0; yNew<newHeight; yNew++) {
-            for (int xNew=0; xNew<newWidth; xNew++) {
-                int posNew = pos(xNew, yNew, newWidth);
-                // Punkt A im Originalbild
-                int aX = (int) Math.round(ratioX * xNew);
-                int aY = (int) Math.round(ratioY * yNew);
-                // h und v berechnen
-                double h = ratioX * xNew - aX;
-                double v = ratioY * yNew -aY;
-                // Farbwerte rgb auslesen
-                rgbA = origPix[pos(aX, aY, origWidth)];
-                if (aX == origWidth-1) {
-                    rgbB = origPix[pos(aX, aY, origWidth)];
-                } else {
-                    rgbB = origPix[pos(aX+1, aY, origWidth)];
-                }
-                if (aY == origHeight-1) {
-                    rgbC = origPix[pos(aX, aY, origWidth)];
-                } else {
-                    rgbC = origPix[pos(aX, aY+1, origWidth)];
-                }
-                if (aX == origWidth-1 && aY == origHeight-1) {
-                    rgbD = origPix[pos(aX, aY, origWidth)];
-                }
-                else if (aX == origWidth-1 && aY != origHeight-1) {
-                    rgbD = origPix[pos(aX, aY+1, origWidth)];
-                }
-                else if (aX != origWidth-1 && aY == origHeight-1) {
-                    rgbD = origPix[pos(aX+1, aY, origWidth)];
-                } else {
-                    rgbD = origPix[pos(aX+1, aY+1, origWidth)];
-                }
-                // r, g, b extrahieren
-                int rA = (rgbA >> 16) & 0xff;
-                int gA = (rgbA >> 8) & 0xff;
-                int bA = rgbA & 0xff;
-                int rB = (rgbB >> 16) & 0xff;
-                int gB = (rgbB >> 8) & 0xff;
-                int bB = rgbB & 0xff;
-                int rC = (rgbC >> 16) & 0xff;
-                int gC = (rgbC >> 8) & 0xff;
-                int bC = rgbC & 0xff;
-                int rD = (rgbD >> 16) & 0xff;
-                int gD = (rgbD >> 8) & 0xff;
-                int bD = rgbD & 0xff;
-                int rNew = (int) Math.round(rA*(1-h)*(1-v) + rB*h*(1-v) + rC*(1-h)*v + rD*h*v);
-                int gNew = (int) Math.round(gA*(1-h)*(1-v) + gB*h*(1-v) + gC*(1-h)*v + gD*h*v);
-                int bNew = (int) Math.round(bA*(1-h)*(1-v) + bB*h*(1-v) + bC*(1-h)*v + bD*h*v);
-                // Werte korrigieren
-                if (rNew > 255) {
-                    rNew = 255;
-                }
-                else if (rNew < 0) {
-                    rNew = 0;
-                }
-                if (gNew > 255) {
-                    gNew = 255;
-                }
-                else if (gNew < 0) {
-                    gNew = 0;
-                }
-                if (bNew > 255) {
-                    bNew = 255;
-                }
-                else if (bNew < 0) {
-                    bNew = 0;
-                }
-                 
-                newPix[posNew] = (0xff<<24) | (rNew<<16) | (gNew<<8) | (bNew);
- 
-            }
-        }
-    }
- 
-    private void nearestNeighbor(int[] origPix, int origWidth, int origHeight,
-            int[] newPix, int newWidth, int newHeight) {
-         
-        // Werte -1, damit nicht über den Rand gelaufen wird
-        double oW = (double) (origWidth-1);
-        double nW = (double) (newWidth-1);
-        double oH = (double) (origHeight-1);
-        double nH = (double) (newHeight-1);
-        double ratioX = oW/nW;
-        double ratioY = oH/nH;
-         
-        // Schleife ueber das neue Bild
-        for (int yNew=0; yNew<newHeight; yNew++) {
-            for (int xNew=0; xNew<newWidth; xNew++) {
-                 
-                // nearest neighbor bestimmen
-                int posNew = pos(xNew, yNew, newWidth);
-                int origX = (int) Math.round(ratioX * xNew);
-                int origY = (int) Math.round(ratioY * yNew);
-                int posOrig = pos(origX, origY, origWidth);
-                 
-                newPix[posNew] = origPix[posOrig];
-            }
-        }
- 
-    }
- 
-    private void copyImage(int[] origPix, int origWidth, int origHeight,
-            int[] newPix, int newWidth, int newHeight) {
-        for (int yNew=0; yNew<newHeight; yNew++) {
-            for (int xNew=0; xNew<newWidth; xNew++) {
-                int y = yNew;
-                int x = xNew;
- 
-                if (y < origHeight && x < origWidth) {
-                    int posNew = yNew*newWidth + xNew;
-                    int pos  =  y  *origWidth   + x;
- 
-                    newPix[posNew] = origPix[pos];
-                }
-             }
-            }
-    }
+	public int setup(String arg, ImagePlus imp) {
+		if (arg.equals("about")) {
+			showAbout();
+			return DONE;
+		}
+		return DOES_RGB + NO_CHANGES;
+		// kann RGB-Bilder und veraendert das Original nicht
+	}
+
+	public void run(ImageProcessor ip) {
+
+		String[] dropdownmenue = { "Kopie", "Pixelwiederholung", "Bilinear" };
+
+		GenericDialog gd = new GenericDialog("scale");
+		gd.addChoice("Methode", dropdownmenue, dropdownmenue[0]);
+		gd.addNumericField("Hoehe:", 500, 0);
+		gd.addNumericField("Breite:", 400, 0);
+
+		gd.showDialog();
+
+		int newHeight = (int) gd.getNextNumber(); // fuer das neue skalierte Bild
+		int newWidth = (int) gd.getNextNumber();  // fuer das neue skalierte Bild
+
+		int width = ip.getWidth(); // Breite bestimmen
+		int height = ip.getHeight(); // Hoehe bestimmen
+
+		ImagePlus neu = NewImage.createRGBImage("Skaliertes Bild", newWidth, newHeight, 1, NewImage.FILL_BLACK); // black für Kopie, falls Bild kleiner als Fläche
+
+		ImageProcessor newIP = neu.getProcessor();
+
+		int[] pix = (int[]) ip.getPixels(); // get original Pixel
+		int[] newPix = (int[]) newIP.getPixels(); // get new Pixels
+
+		double scaleHeight = height / (newHeight * 1.0);
+		double scaleWidth = width / (newWidth * 1.0);
+
+		String method = gd.getNextChoice();
+
+
+		if (method.equals("Kopie")) {
+			// Schleife ueber das neue Bild
+			for (int newY = 0; newY < newHeight; newY++) {
+				for (int newX = 0; newX < newWidth; newX++) {
+					int y = newY;
+					int x = newX;
+
+					if (y < height && x < width) {
+						int newPos = newY * newWidth + newX;
+						int pos = y * width + x;
+
+						newPix[newPos] = pix[pos];
+					}
+				}
+			}
+		}
+
+		// http://home.htw-berlin.de/~barthel/veranstaltungen/GLDM/vorlesungen/06_GLDM_Bildmanipulation3_geometrische.pdf
+		if (method.equals("Pixelwiederholung")) {
+
+			// Schleife ueber das neue Bild
+			for (int newY = 0; newY < newHeight; newY++) {
+				for (int newX = 0; newX < newWidth; newX++) {
+					
+					// gerundete Werte für x und y
+					int x = (int) Math.round(newX * scaleWidth);
+					int y = (int) Math.round(newY * scaleHeight);
+
+					if (y < height && x < width) {
+						int newPos = newY * newWidth + newX;
+						int pos = y * width + x;
+
+						newPix[newPos] = pix[pos];
+
+					}
+				}
+			}
+		}
+
+		if (method.equals("Bilinear")) {
+			int PointA, PointB, PointC, PointD;
+			
+			// Skalierungswerte, brauchte height-1 und width-1 sonst IndexOutOfBounds-Fehler
+			
+			scaleHeight = (height - 1) / (newHeight * 1.0);
+			scaleWidth = (width - 1) / (newWidth * 1.0);
+			// Schleife ueber das neue Bild
+			for (int newY = 0; newY < newHeight; newY++) {
+				for (int newX = 0; newX < newWidth; newX++) {
+					int y = (int) (newY * scaleHeight);
+					int x = (int) (newX * scaleWidth);
+
+					// v und h sind die Nachkommastelle vom skalierten Y-Wert/X-Wert
+					double v = (newY * scaleHeight) - y;
+					double h = (newX * scaleWidth) - x;
+
+					// Alte Position
+					int position = y * width + x;
+
+					// Die Punkte A, B, C und D und ihre ARGB-Werte
+					PointA = pix[position];
+					int[] argbA = { ((PointA >> 16) & 0xff), ((PointA >> 8) & 0xff), (PointA & 0xff) };
+					PointB = pix[position + 1];
+					int[] argbB = { ((PointB >> 16) & 0xff), ((PointB >> 8) & 0xff), (PointB & 0xff) };
+					PointC = pix[position + width];
+					int[] argbC = { ((PointC >> 16) & 0xff), ((PointC >> 8) & 0xff), (PointC & 0xff) };
+					PointD = pix[position + width + 1];
+					int[] argbD = { ((PointD >> 16) & 0xff), ((PointD >> 8) & 0xff), (PointD & 0xff) };
+
+					int r, g, b;
+
+					// Formel aus: http://home.htw-berlin.de/~barthel/veranstaltungen/GLDM/vorlesungen/07_GLDM_Bildmanipulation3_geometrische_2.pdf
+					// bzw. http://home.htw-berlin.de/~barthel/veranstaltungen/GLDM/vorlesungen/06_GLDM_Bildmanipulation3_geometrische.pdf
+					// P = A * (1-h) * (1-V) + B * h * (1-v) + C * (1-h) * v + D * h * v
+					r = (int) (argbA[0] * (1 - h) * (1 - v) + argbB[0] * h * (1 - v) + argbC[0] * (1 - h) * v
+							+ argbD[0] * h * v);
+					g = (int) (argbA[1] * (1 - h) * (1 - v) + argbB[1] * h * (1 - v) + argbC[1] * (1 - h) * v
+							+ argbD[1] * h * v);
+					b = (int) (argbA[2] * (1 - h) * (1 - v) + argbB[2] * h * (1 - v) + argbC[2] * (1 - h) * v
+							+ argbD[2] * h * v);
+
+
+					// Berechnung der neuen Position
+					int newPos = newY * newWidth + newX;
+
+					// Zurückschreiben der Werte
+					newPix[newPos] = 0xFF << 24 | r << 16  | g << 8  | b;
+
+				}
+			}
+		}
+
+		// neues Bild anzeigen
+		neu.show();
+		neu.updateAndDraw();
+	}
 
 	void showAbout() {
 		IJ.showMessage("");
 	}
 }
-
